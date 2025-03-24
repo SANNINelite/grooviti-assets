939532c176998d4211e7154dab09d1f21da1e2dc
Swaroop Mane <149792123+SANNINelite@users.noreply.github.com>
2025-03-24 14:16:44 +0530

    Add files via upload

---
+++ assets.js
@@ -0,0 +1,356 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import header from './header.png'
+import background from './background.png'
+import search_icon from './search_icon.png'
+import event_1 from './event_1.png'
+import event_2 from './event_2.png'
+import event_3 from './event_3.png'
+import event_4 from './event_4.png'
+import event_5 from './event_5.png'
+import event_6 from './event_6.png'
+import event_7 from './event_7.png'
+import event_8 from './event_8.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import instagram_icon from './instagram_icon.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    header,
+    background,
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    instagram_icon,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const event_list = [
+    {
+        event_name: "Cultural",
+        event_image: event_1
+    },
+    {
+        event_name: "Club",
+        event_image: event_2
+    },
+    {
+        event_name: "Sports",
+        event_image: event_3
+    },
+    {
+        event_name: "Tech",
+        event_image: event_4
+    },
+    {
+        event_name: "Drama",
+        event_image: event_5
+    },
+    {
+        event_name: "Open-mic",
+        event_image: event_6
+    },
+    {
+        event_name: "Stand-up",
+        event_image: event_7
+    },
+    {
+        event_name: "Conference",
+        event_image: event_8
+    }]
+
+export const myevents_list = [
+    {
+        _id: "1",
+        name: "UTKARSH",
+        image: food_1,
+        price: 199,
+        description: "📅 March 23, 2025 | 🕒 12:00 AM | Pccoer Campus",
+        category: "Tech"
+    },
+    {
+        _id: "2",
+        name: "Powada Competition",
+        image: food_2,
+        price: 199,
+        description: "LOREM",
+        category: "Cultural"
+    }, {
+        _id: "3",
+        name: "Folk Dance",
+        image: food_3,
+        price: 199,
+        description: "lorem",
+        category: "Cultural"
+    }, {
+        _id: "4",
+        name: "Music",
+        image: food_4,
+        price: 99,
+        description: "lorem",
+        category: "Cultural"
+    }, {
+        _id: "5",
+        name: "Dj Night",
+        image: food_5,
+        price: 140,
+        description: "lorem",
+        category: "Club"
+    }, {
+        _id: "6",
+        name: "Clubbing",
+        image: food_6,
+        price: 120,
+        description: "description",
+        category: "Club"
+    }, {
+        _id: "7",
+        name: "Food-Festival",
+        image: food_7,
+        price: 200,
+        description: "lorem",
+        category: "Club"
+    }, {
+        _id: "8",
+        name: "Pool-Party",
+        image: food_8,
+        price: 150,
+        description: "lorem",
+        category: "Club"
+    }, {
+        _id: "9",
+        name: "Cricket",
+        image: food_9,
+        price: 140,
+        description: "lorem",
+        category: "Sports"
+    }, {
+        _id: "10",
+        name: "Football",
+        image: food_10,
+        price: 220,
+        description: "lorem",
+        category: "Sports"
+    }, {
+        _id: "11",
+        name: "Basketball",
+        image: food_11,
+        price: 100,
+        description: "lorem",
+        category: "Sports"
+    }, {
+        _id: "12",
+        name: "Badminton",
+        image: food_12,
+        price: 120,
+        description: "lorem",
+        category: "Sports"
+    },
+    {
+        _id: "13",
+        name: "Cultural-event",
+        image: food_13,
+        price: 129,
+        description: "lorem",
+        category: "Cultural"
+    },
+    {
+        _id: "14",
+        name: "Hackathon",
+        image: food_14,
+        price: 189,
+        description: "lorem",
+        category: "Tech"
+    }, {
+        _id: "15",
+        name: "GDSC",
+        image: food_15,
+        price: 169,
+        description: "lorem",
+        category: "Tech"
+    }, {
+        _id: "16",
+        name: "ACM",
+        image: food_16,
+        price: 249,
+        description: "lorem",
+        category: "Tech"
+    }, {
+        _id: "17",
+        name: "Natak",
+        image: food_17,
+        price: 149,
+        description: "lorem",
+        category: "Drama"
+    }, {
+        _id: "18",
+        name: "Ekankika",
+        image: food_18,
+        price: 120,
+        description: "lorem",
+        category: "Drama"
+    }, {
+        _id: "19",
+        name: "Ekankika 2.0",
+        image: food_19,
+        price: 200,
+        description: "lorem",
+        category: "Drama"
+    }, {
+        _id: "20",
+        name: "Drama 2.0",
+        image: food_20,
+        price: 159,
+        description: "lorem",
+        category: "Drama"
+    }, {
+        _id: "21",
+        name: "Comedy-laughter ",
+        image: food_21,
+        price: 149,
+        description: "lorem",
+        category: "Open-mic"
+    }, {
+        _id: "22",
+        name: "LOL",
+        image: food_22,
+        price: 229,
+        description: "lorem",
+        category: "Open-mic"
+    }, {
+        _id: "23",
+        name: "India's Got Latent",
+        image: food_23,
+        price: 109,
+        description: "lorem",
+        category: "Open-mic"
+    }, {
+        _id: "24",
+        name: "LOL 2.0",
+        image: food_24,
+        price: 129,
+        description: "lorem",
+        category: "Open-mic"
+    },
+    {
+        _id: "25",
+        name: "Samay Raina",
+        image: food_25,
+        price: 120,
+        description: "lorem",
+        category: "Stand-up"
+    },
+    {
+        _id: "26",
+        name: "Anubhav Singh Bassi",
+        image: food_26,
+        price: 180,
+        description: "lorem",
+        category: "Stand-up"
+    }, {
+        _id: "27",
+        name: "Kullu",
+        image: food_27,
+        price: 106,
+        description: "lorem",
+        category: "Stand-up"
+    }, {
+        _id: "28",
+        name: "Tanmay Bhat",
+        image: food_28,
+        price: 240,
+        description: "lorem",
+        category: "Stand-up"
+    }, {
+        _id: "29",
+        name: "Start UP",
+        image: food_29,
+        price: 140,
+        description: "lorem",
+        category: "Conference"
+    }, {
+        _id: "30",
+        name: "GDSC",
+        image: food_30,
+        price: 120,
+        description: "lorem",
+        category: "Conference"
+    }, {
+        _id: "31",
+        name: "Forum",
+        image: food_31,
+        price: 200,
+        description: "lorem",
+        category: "Conference"
+    }, {
+        _id: "32",
+        name: "Event",
+        image: food_32,
+        price: 150,
+        description: "lorem",
+        category: "Conference"
+    }
+]
